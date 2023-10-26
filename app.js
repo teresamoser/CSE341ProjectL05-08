a0c32e78cffabd2c6dab5ce9d245ae5cbae5e751
Teresa Moser <moserte@byui.edu>
2023-10-25 22:41:47 -0600

    save login code

--- app.js
+++ app.js
@@ -1,52 +1,101 @@
-// const express = require('express');
-// const cors = require ('cors');
-// const app = express();
-
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
-
-// const bodyParser = require('body-parser');
-// const mongodb = require('./lesson02/db/connect');
-
-// app
-//   .use('/api-docs', swaggerUi.service, swaggerUi.setup(swaggerDocument))
-//   .use(cors())
-//   .use(express.json())
-//   .use(express.urlencoded({extended: true}))
-//   .use('./', require('./planetsProject/routes'));
-
-
-  // .use(bodyParser.json())
-  // .use((req, res, next) =>{
-  //   res.setHeader('Access-Control-Allow-Origin', '*');
-  //   next();
-  // })
-
-// const db = require('./models');
-//   db.mongoose
-//   .connect(db.url,{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log('Connected to the Database!');
-//   })
-//   .catch((err) => {
-//     console.log('Cannot connect to the Database!', err);
-//     process.exit();
-//   });
-
-  // const PORT = process.env.PORT || 8080;
-  // app.listen(PORT, () => {
-  //   console.log(`Server is running on ${PORT}.`)
-  // });
+var express = require("express"), 
+    mongoose = require("mongoose"), 
+    passport = require("passport"), 
+    bodyParser = require("body-parser"), 
+    LocalStrategy = require("passport-local"), 
+    passportLocalMongoose =  
+        require("passport-local-mongoose") 
+const User = require("./model/User"); 
+var app = express(); 
   
-
-// mongodb.initDb((err, mongodb) => {
-//   if (err) {
-//     console.log(err);
-//   }else {
-//     app.listen(port);
-//     console.log(`Connected to DB and listening on ${port}`);
-//   }
-//   });
+mongoose.connect("mongodb://localhost/27017"); 
+  
+app.set("view engine", "ejs"); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(require("express-session")({ 
+    secret: "Rusty is a dog", 
+    resave: false, 
+    saveUninitialized: false
+})); 
+  
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+  
+passport.use(new LocalStrategy(User.authenticate())); 
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser()); 
+  
+//===================== 
+// ROUTES 
+//===================== 
+  
+// Showing home page 
+app.get("/", function (req, res) { 
+    res.render("home"); 
+}); 
+  
+// Showing secret page 
+app.get("/secret", isLoggedIn, function (req, res) { 
+    res.render("secret"); 
+}); 
+  
+// Showing register form 
+app.get("/register", function (req, res) { 
+    res.render("register"); 
+}); 
+  
+// Handling user signup 
+app.post("/register", async (req, res) => { 
+    const user = await User.create({ 
+      username: req.body.username, 
+      password: req.body.password 
+    }); 
+    
+    return res.status(200).json(user); 
+  }); 
+  
+//Showing login form 
+app.get("/login", function (req, res) { 
+    res.render("login"); 
+}); 
+  
+//Handling user login 
+app.post("/login", async function(req, res){ 
+    try { 
+        // check if the user exists 
+        const user = await User.findOne({ username: req.body.username }); 
+        if (user) { 
+          //check if password matches 
+          const result = req.body.password === user.password; 
+          if (result) { 
+            res.render("secret"); 
+          } else { 
+            res.status(400).json({ error: "password doesn't match" }); 
+          } 
+        } else { 
+          res.status(400).json({ error: "User doesn't exist" }); 
+        } 
+      } catch (error) { 
+        res.status(400).json({ error }); 
+      } 
+}); 
+  
+//Handling user logout  
+app.get("/logout", function (req, res) { 
+    req.logout(function(err) { 
+        if (err) { return next(err); } 
+        res.redirect('/'); 
+      }); 
+}); 
+  
+  
+  
+function isLoggedIn(req, res, next) { 
+    if (req.isAuthenticated()) return next(); 
+    res.redirect("/login"); 
+} 
+  
+var port = process.env.PORT || 3000; 
+app.listen(port, function () { 
+    console.log("Server Has Started!"); 
+}); 
