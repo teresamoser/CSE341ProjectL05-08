85cf66f2bf61bd8ee8cdc235ea355005c947cf13
Teresa Moser <moserte@byui.edu>
2023-11-02 21:29:43 -0600

    Auth0 files

--- planetsProject/routes/index.js
+++ planetsProject/routes/index.js
@@ -26,3 +26,36 @@ app.use(auth(config));
 app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
+
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+var request = require("request");
+
+var options = { method: 'POST',
+  url: 'https://dev-ub36gl36y4obcwli.us.auth0.com/oauth/token',
+  headers: { 'content-type': 'application/json' },
+  body: '{"client_id":"Kg04PeSvvRxbnWDQ3dBQfng6PMAkdWAD","client_secret":"yc4UYyNwuvNc7Y9uNQGItPNZbRaAv5nlWXEWusq317dhYhpdb48q3B5yuasyAJuj","audience":"https://dev-ub36gl36y4obcwli.us.auth0.com/api/v2/","grant_type":"client_credentials"}' };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
+var axios = require("axios").default;
+
+var options = {
+  method: 'GET',
+  url: 'https://dev-ub36gl36y4obcwli.us.auth0.com/api/v2/',
+  headers: {authorization: 'Bearer TOKEN'}
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
