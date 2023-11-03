3466e5e0f98fa254e1e436308a2973d4afffc718
Teresa Moser <moserte@byui.edu>
2023-11-02 20:14:27 -0600

    Save AuthO files

--- planetsProject/routes/index.js
+++ planetsProject/routes/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
 
 router.use('/', require('./swagger'));
 router.use('/planets', require('./planets'));
 router.use('/spacecraft', require('./spacecraft'));
 
 module.exports = router;
+
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
