2066237f7c2a3134dc6cefb4dda1ee7b089af637
Teresa Moser <moserte@byui.edu>
2023-11-02 21:33:33 -0600

    format fix

--- planetsProject/routes/index.js
+++ planetsProject/routes/index.js
@@ -38,7 +38,8 @@ var request = require("request");
 var options = { method: 'POST',
   url: 'https://dev-ub36gl36y4obcwli.us.auth0.com/oauth/token',
   headers: { 'content-type': 'application/json' },
-  body: '{"client_id":"Kg04PeSvvRxbnWDQ3dBQfng6PMAkdWAD","client_secret":"yc4UYyNwuvNc7Y9uNQGItPNZbRaAv5nlWXEWusq317dhYhpdb48q3B5yuasyAJuj","audience":"https://dev-ub36gl36y4obcwli.us.auth0.com/api/v2/","grant_type":"client_credentials"}' };
+  body: '{"client_id":"Kg04PeSvvRxbnWDQ3dBQfng6PMAkdWAD","client_secret":"yc4UYyNwuvNc7Y9uNQGItPNZbRaAv5nlWXEWusq317dhYhpdb48q3B5yuasyAJuj","audience":"https://dev-ub36gl36y4obcwli.us.auth0.com/api/v2/","grant_type":"client_credentials"}' 
+    };
 
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
