466f74680c5fabb3d70461c4f886da8910677d5c
Teresa Moser <moserte@byui.edu>
2023-11-09 22:39:09 -0700

    save sign in validation for user

--- planetsProject/routes/index.js
+++ planetsProject/routes/index.js
@@ -1,28 +1,71 @@
 const express = require('express');
-const router = express.Router();
+// const router = express.Router();
 const app = express();
+
+var router = require('express').Router();
+const { auth, requiresAuth } = require('express-openid-connect');
+const dotenv = require ('dotenv');
 
 router.use('/', require('./swagger'));
 router.use('/planets', require('./planets'));
 router.use('/spacecraft', require('./spacecraft'));
 
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Auth0 Webapp sample Nodejs',
+    isAuthenticated: req.oidc.isAuthenticated()
+  });
+});
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page'
+  });
+});
+
 module.exports = router;
 
-const { auth } = require('express-openid-connect');
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
+const createError = require('http-errors');
+//const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { signupValidation, loginValidation } = require('../middleware/validate.js');
+const port = 8080; 
+//const app = express();
+ 
+app.use(express.json());
+ 
+app.use(bodyParser.json());
+ 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
+app.use(cors());
+ 
 app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    res.send('Node js file upload rest apis');
 });
+ 
+app.post('/register', signupValidation, (req, res, next) => {
+   // your registration code
+});
+ 
+ 
+app.post('/login', loginValidation, (req, res, next) => {
+   // your login code
+});
+ 
+// Handling Errors
+app.use((err, req, res, next) => {
+    // console.log(err);
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+    res.status(err.statusCode).json({
+      message: err.message,
+    });
+});
+ 
+app.listen(port,() => console.log(`Server is running on port ${port}`));
