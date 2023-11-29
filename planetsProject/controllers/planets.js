e5cf541cacbec9cded2520deb4d3d480703eb826
Teresa Moser <moserte@byui.edu>
2023-11-28 22:20:37 -0700

    engage auth0; prettier fixes

--- planetsProject/controllers/planets.js
+++ planetsProject/controllers/planets.js
@@ -1,33 +1,27 @@
 // Importing Dependencies:
 const mongodb = require('../db/connect'); // is a reference to my database connection
-const ObjectId = require('mongodb').ObjectId;  // is a type provided by the MongoDB driver. Allows me to work with MongoDB's unique identifiers.
+const ObjectId = require('mongodb').ObjectId; // is a type provided by the MongoDB driver. Allows me to work with MongoDB's unique identifiers.
 
 // Centralized error response function
 function errorResponse(res, statusCode, message) {
   return res.status(statusCode).json({ error: message });
-};
-
-// Function that handles a GET request.
+}
+//Function that handles a GET request.
 const getAllPlanets = async (req, res) => {
-  try{
+  try {
     // add the database
-    const result = await mongodb
-      .getDb()
-      .db()
-      .collection('planets')
-      .find()
-      .toArray();
+    const result = await mongodb.getDb().db().collection('planets').find().toArray();
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
-  } catch (error){
+  } catch (error) {
     console.error(error);
-    errorResponse(res, 500, 'Internal Server Error'); 
+    errorResponse(res, 500, 'Internal Server Error');
   }
 };
 
 // Gets a single Spacecraft
 const getSinglePlanet = async (req, res) => {
-  try{
+  try {
     //add the database
     const planetId = new ObjectId(req.params.id);
     const result = await mongodb
@@ -36,17 +30,17 @@ const getSinglePlanet = async (req, res) => {
       .collection('planets')
       .find({ _id: planetId })
       .toArray();
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(result[0]);
-    } catch (error) {
-      console.error(error);
-      errorResponse(res, 500, 'Internal Server Error');
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result[0]);
+  } catch (error) {
+    console.error(error);
+    errorResponse(res, 500, 'Internal Server Error');
   }
 };
 
 // Create a POST const
 const createPlanets = async (req, res) => {
-  try{
+  try {
     // Check if user is authenticated (To be used with OAuth)
     // if (!req.oidc.isAuthenticated()) {
     //   return errorResponse(res, 401, 'Unauthorized. Please login to schedule an appointment.');
@@ -54,29 +48,23 @@ const createPlanets = async (req, res) => {
 
     // add the database
     const planets = req.body;
-    const response = await mongodb
-      .getDb()
-      .db()
-      .collection('planets')
-      .insertOne(planets);
+    const response = await mongodb.getDb().db().collection('planets').insertOne(planets);
 
     if (response.acknowledged) {
       const newPlanetId = response.insertedId;
-      res
-      .status(201)
-      .json({ message: 'Planet created successfully', PlanetId: newPlanetId });
-      } else {
-        errorResponse(res, 500, 'Failed to create Planet');
-      }
-    } catch (error) {
-      console.error(error);
-      errorResponse(res, 500, 'Internal Server Error');
-  } 
+      res.status(201).json({ message: 'Planet created successfully', PlanetId: newPlanetId });
+    } else {
+      errorResponse(res, 500, 'Failed to create Planet');
+    }
+  } catch (error) {
+    console.error(error);
+    errorResponse(res, 500, 'Internal Server Error');
+  }
 };
 
 // Function that handles a PUT request to update a spacecraft.
 const updatePlanets = async (req, res) => {
-  try{
+  try {
     // Check if user is authenticated
     // if (!req.oidc.isAuthenticated()) {
     //   return errorResponse(res, 401, 'Unauthorized. Please login to schedule a appointment.');
@@ -90,22 +78,21 @@ const updatePlanets = async (req, res) => {
       .db()
       .collection('planets')
       .updateOne({ _id: planetId }, { $set: { planet: updatedPlanet } });
-   
-      if (response.matchedCount === 1 && response.modifiedCount === 1) {
-        res.status(204).json({ message: 'Planet updated successfully' });
-        } else {
-          errorResponse(res, 404, 'Planet not found or not updated');
-        }
-      } catch (error) {
-        console.error(error);
-        errorResponse(res, 500, 'Internal Server Error');
-    }
-};
 
+    if (response.matchedCount === 1 && response.modifiedCount === 1) {
+      res.status(204).json({ message: 'Planet updated successfully' });
+    } else {
+      errorResponse(res, 404, 'Planet not found or not updated');
+    }
+  } catch (error) {
+    console.error(error);
+    errorResponse(res, 500, 'Internal Server Error');
+  }
+};
 
 // Function that handles a DELETE request.
 const deletePlanets = async (req, res) => {
-  try{
+  try {
     // Check if user is authenticated
     // if (!req.oidc.isAuthenticated()) {
     //   return errorResponse(res, 401, 'Unauthorized. Please login to delete an appointment.');
@@ -113,20 +100,16 @@ const deletePlanets = async (req, res) => {
 
     // add the database
     const planetId = new ObjectId(req.params.id);
-    const response = await mongodb
-      .getDb()
-      .db()
-      .collection('planets')
-      .deleteOne({ _id: planetId });
+    const response = await mongodb.getDb().db().collection('planets').deleteOne({ _id: planetId });
 
     if (response.deletedCount === 1) {
       res.status(200).json({ message: 'Planet deleted successfully' });
-      } else {
-        errorResponse(res, 404, 'Planet not found');
-      }
-    } catch (error) {
-      console.error(error);
-      errorResponse(res, 500, 'Internal Server Error');
+    } else {
+      errorResponse(res, 404, 'Planet not found');
+    }
+  } catch (error) {
+    console.error(error);
+    errorResponse(res, 500, 'Internal Server Error');
   }
 };
 
--- planetsProject/controllers/spacecraft.js
+++ planetsProject/controllers/spacecraft.js
@@ -1,25 +1,19 @@
 // Importing Dependencies:
-const mongodb = require('../db/connect');// is a reference to my database connection
-const ObjectId = require('mongodb').ObjectId;// is a type provided by the MongoDB driver. Allows me to work with MongoDB's unique identifiers.
+const mongodb = require('../db/connect'); // is a reference to my database connection
+const ObjectId = require('mongodb').ObjectId; // is a type provided by the MongoDB driver. Allows me to work with MongoDB's unique identifiers.
 
 // Centralized error response function
 function errorResponse(res, statusCode, message) {
   return res.status(statusCode).json({ error: message });
-};
-
+}
 // Function that handles a GET request.
 const getAllSpacecraft = async (req, res) => {
-  try{
+  try {
     // add the database
-    const result = await mongodb
-      .getDb()
-      .db()
-      .collection('spacecraft')
-      .find()
-      .toArray();
+    const result = await mongodb.getDb().db().collection('spacecraft').find().toArray();
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
-  } catch (error){
+  } catch (error) {
     console.error(error);
     errorResponse(res, 500, 'Internal Server Error');
   }
@@ -27,7 +21,7 @@ const getAllSpacecraft = async (req, res) => {
 
 // Gets a single Spacecraft
 const getSingleSpacecraft = async (req, res) => {
-  try{
+  try {
     // add the database
     const spacecraftId = new ObjectId(req.params.id);
     const result = await mongodb
@@ -44,30 +38,25 @@ const getSingleSpacecraft = async (req, res) => {
   }
 };
 
-
 // Create a POST const
 const createSpacecraft = async (req, res) => {
-  try{
+  try {
     // Check if user is authenticated (To be used with OAuth)
     // if (!req.oidc.isAuthenticated()) {
     //   return errorResponse(res, 401, 'Unauthorized. Please login to schedule an appointment.');
     // }
     // add the database
     const spacecraft = req.body;
-    const response = await mongodb
-      .getDb()
-      .db()
-      .collection('spacecraft')
-      .insertOne(spacecraft);
+    const response = await mongodb.getDb().db().collection('spacecraft').insertOne(spacecraft);
 
     if (response.acknowledged) {
       const newSpacecraftId = response.insertedId;
-        res
+      res
         .status(201)
         .json({ message: 'Spacecraft created successfully', SpacecraftId: newSpacecraftId });
-      } else {
-        errorResponse(res, 500, 'Failed to create Spacecraft');
-      }
+    } else {
+      errorResponse(res, 500, 'Failed to create Spacecraft');
+    }
   } catch (error) {
     console.error(error);
     errorResponse(res, 500, 'Internal Server Error');
@@ -92,18 +81,18 @@ const updateSpacecraft = async (req, res) => {
 
     if (response.matchedCount === 1 && response.modifiedCount === 1) {
       res.status(204).json({ message: 'Spacecraft updated successfully' });
-      } else {
-        errorResponse(res, 404, 'Spacecraft not found or not updated');
-      }
-    } catch (error) {
-      console.error(error);
-      errorResponse(res, 500, 'Internal Server Error');
+    } else {
+      errorResponse(res, 404, 'Spacecraft not found or not updated');
+    }
+  } catch (error) {
+    console.error(error);
+    errorResponse(res, 500, 'Internal Server Error');
   }
 };
 
 // Function that handles a DELETE request.
 const deleteSpacecraft = async (req, res) => {
-  try{
+  try {
     // Check if user is authenticated
     // if (!req.oidc.isAuthenticated()) {
     //   return errorResponse(res, 401, 'Unauthorized. Please login to delete an appointment.');
@@ -118,12 +107,12 @@ const deleteSpacecraft = async (req, res) => {
 
     if (response.deletedCount === 1) {
       res.status(200).json({ message: 'Spacecraft deleted successfully' });
-      } else {
-        errorResponse(res, 404, 'Spacecraft not found');
-      }
-    } catch (error) {
-      console.error(error);
-      errorResponse(res, 500, 'Internal Server Error');
+    } else {
+      errorResponse(res, 404, 'Spacecraft not found');
+    }
+  } catch (error) {
+    console.error(error);
+    errorResponse(res, 500, 'Internal Server Error');
   }
 };
 
