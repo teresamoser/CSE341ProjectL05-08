a39429045bfef6ef5cc98666469319db9dc52ac0
Teresa Moser <moserte@byui.edu>
2023-12-02 23:32:21 -0700

    Remove 01 login sample files

--- planetsProject/routes/index.js
+++ planetsProject/routes/index.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const app = express();
-// DO NOT UNCOMMENT until Auth0 is working
-// const { auth } = require('express-openid-connect');
-// const auth0Controller = require('../controllers/auth0');
+const { auth } = require('express-openid-connect');
+const auth0Controller = require('../controllers/auth0');
 const homeController = require('../controllers/index');
 
 const swagger = require('./swagger');
 const planets = require('./planets');
 const spacecraft = require('./spacecraft');
-// const users = require('./users');
-// const auth0 = require('./auth0');
+//const users = require('./users');
+const auth0 = require('./auth0');
 
+router.use(auth(auth0Controller.config));
+router.use('/auth0', auth0);
 router.use('/api-docs', swagger);
 router.use('/planets', planets);
 router.use('/spacecraft', spacecraft);
 // router.use('/users', users);
 router.use('/', homeController.home);
 
-// DO NOT UNCOMMENT until Auth0 is working
-// router.use(auth(auth0Controller.config));
-// router.use('/auth0', auth0);
 module.exports = router;
