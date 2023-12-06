3195b6bcba2d4ea29eeb7b5797701dc3ef2ccabd
Teresa Moser <moserte@byui.edu>
2023-12-05 22:14:12 -0700

    Save updates to server.js

--- planetsProject/routes/index.js
+++ planetsProject/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const router = express.Router();
 const { auth } = require('express-openid-connect');
+const router = express.Router();
 const auth0Controller = require('../controllers/auth0');
 const homeController = require('../controllers/index');
 
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT;
 
 const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
+const checkScopes = requiredScopes('read:messages');
 
 // Authorization middleware. When used, the Access Token must
 // exist and be verified against the Auth0 JSON Web Key Set.
@@ -13,42 +14,6 @@ const checkJwt = auth({
   audience: 'https://dev-5bq8e5nmcxqqmalf.us.auth0.com/api/v2/',
   issuerBaseURL: 'http://localhost:8080'
 });
-
-// This route doesn't need authentication
-app.get('/api/public', function (req, res) {
-  res.json({
-    message: 'Hello from a public endpoint! You do not need to be authenticated to see this.'
-  });
-});
-
-// This route needs authentication
-app.get('/api/private', checkJwt, function (req, res) {
-  res.json({
-    message: 'Hello from a private endpoint! You need to be authenticated to see this.'
-  });
-});
-
-const checkScopes = requiredScopes('read:messages');
-
-app.get('/api/private-scoped', checkJwt, checkScopes, function (req, res) {
-  res.json({
-    message:
-      'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
-  });
-});
-
-app.listen(3000, function () {
-  console.log('Listening on http://localhost:8080');
-});
-
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    // Allows access from any website
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./planetsProject/routes'));
 
 mongodb.initDb((err) => {
   if (err) {
@@ -58,3 +23,26 @@ mongodb.initDb((err) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
+
+// This route doesn't need authentication
+app.get('/api/public', function (req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You do not need to be authenticated to see this.'
+  });
+});
+
+app.get('/api/private-scoped', checkJwt, checkScopes, function (_req, res) {
+  res.json({
+    message:
+      'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+  });
+});
+
+app
+  .use(bodyParser.json())
+  .use((_req, res, next) => {
+    // Allows access from any website
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./planetsProject/routes'));
